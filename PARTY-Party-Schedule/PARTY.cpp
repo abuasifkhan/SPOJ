04714b48a9987376909c3e2eb4f2a45074ffd752
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-11-12 12:46:43 +0600

    Create PARTY.cpp

---
+++ PARTY-Party-Schedule/PARTY.cpp
@@ -0,0 +1,92 @@
+//! Bismillahi-Rahamanirahim.
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pii pair <int, int>
+#define pb push_back
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define inf (1<<28)
+#define ll long long
+#define mod 1000000007
+#define debug cout<<"ok"<<endl;
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define fs first
+#define sc second
+#define mx 110
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
+// int X[]= {-1, 0, 1, 0};   //x 4 direction
+// int Y[]= { 0, 1, 0, -1};  //y 4 direction
+
+int M, n;
+int dp[3][510];
+int price,fun;
+
+int main() {
+    #ifdef LOCAL
+    open
+    double st=clock(),en;
+    #endif // LOCAL
+
+    while(~scanf("%d %d",&M,&n)){
+        if(M+n==0)break;
+        CLEAR(dp,0);
+        for(int i=1;i<=n;i++){
+            read(price);read(fun);
+            int v=i%2, u=(i-1)%2;
+            for(int w=0;w<=M;w++){
+                if(price>w)
+                    dp[v][w]=dp[u][w];
+                else dp[v][w]=max(dp[u][w], dp[u][w-price]+fun);
+            }
+        }
+        int v=n%2;
+        int ans=dp[v][M],t;
+        for(t=M;t>=0;t--){
+            if(dp[v][t]<ans)break;
+        }
+        printf("%d %d\n",t+1,ans);
+    }
+
+    #ifdef LOCAL
+    en=clock();
+    cout<<"Time: ";
+    cerr<<(double)(en-st)/CLOCKS_PER_SEC<<endl;
+    #endif // LOCAL
+    return 0;
+}
+
+
+
+
