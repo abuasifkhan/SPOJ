6162e5f2d09745b233a9a2c12df9d17dbfff8f9d
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-11-12 13:05:52 +0600

    Create COINS.cpp

---
+++ COINS-Bytelandian-gold-coins/COINS.cpp
@@ -0,0 +1,87 @@
+//! Bismillahi-Rahamanirahim.
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pii pair <int, int>
+#define pb push_back
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define inf (1<<28)
+#define ll long long
+#define mod 1000000007
+#define debug cout<<"ok"<<endl;
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define fs first
+#define sc second
+#define mx 100010
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
+// int X[]= {-1, 0, 1, 0};   //x 4 direction
+// int Y[]= { 0, 1, 0, -1};  //y 4 direction
+
+ll n;
+map<ll,ll>dp;
+
+ll rec(ll num){
+    if(num==0)return 0;
+    if(num==1)return 1;
+    if(dp.count(num))return dp[num];
+    ll ret=num;
+    ret=max(ret, rec(num/2)+rec(num/3)+rec(num/4));
+    dp[num]=ret;
+    return dp[num];
+}
+
+int main() {
+    #ifdef LOCAL
+    open
+    double st=clock(),en;
+    #endif // LOCAL
+
+    while(~scanf("%lld",&n)){
+        dp.clear();
+        ll ans= rec(n);
+        printf("%lld\n",ans);
+    }
+
+    #ifdef LOCAL
+    en=clock();
+    cout<<"Time: ";
+    cerr<<(double)(en-st)/CLOCKS_PER_SEC<<endl;
+    #endif // LOCAL
+    return 0;
+}
+
+
+
+
